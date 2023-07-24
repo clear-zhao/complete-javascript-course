7348295975016931a8c5d75fa373680f80f8db0e
clear-zhao <1289134886@qq.com>
2023-07-24 22:10:44 +0800

    day6

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -4,6 +4,18 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+for (let flight of flights.split('+')) {
+  // console.log(flight);
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} ${time.replace(':', 'h')}.padStart(36)`;
+  console.log(output);
+}
+
 const openingHours = {
   thu: {
     open: 12,
@@ -282,3 +294,24 @@ for (const [time, event] of gameEvents) {
     `${time <= 45 ? '[FIRST HALF]' : '[SECOND HALF]'}${time}:${event}`
   );
 }
+
+// challenge 4
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', () => {
+  const text = document.querySelector('textarea').value;
+  console.log(text);
+  const textArr = text.split('\n');
+  console.log(textArr);
+  let cnt = 1;
+  for (let item of textArr) {
+    let itemArr = item.split('_');
+    let newItem = itemArr[0];
+    for (let i = 1; i < itemArr.length; i++) {
+      newItem += itemArr[i][0].toUpperCase() + itemArr[i].slice(1);
+    }
+    console.log(`${newItem}${'✔'.repeat(cnt)}`);
+    cnt++;
+  }
+});
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const greet = greeting => name => console.log(`${greeting}! ${name}`);
+greet('h1')('zkl');
+
+const beijindaxing = {
+  airline: 'BeiJinDaXing',
+  iataCode: 'BJDX',
+  planes: 300,
+  buPlane: function () {
+    this.planes++;
+    console.log(this.planes);
+  },
+};
+
+// document.querySelector('.buy').addEventListener('click', beijindaxing.buPlane); // result = NaN,因为this默认指向调用它的元素，在这里是document.querySelector('.buy'),它里面没有plans属性，所有使用bind自定义属性，使用bind是因为bind返回一个函数，而这里需要一个函数作为参数。
+document
+  .querySelector('.buy')
+  .addEventListener('click', beijindaxing.buPlane.bind(beijindaxing));
