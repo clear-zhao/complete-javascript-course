f903dc326f98ab8aba65a799cf500c5c8ee7d66c
clear-zhao <1289134886@qq.com>
2023-08-04 16:23:56 +0800

    day11

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -18,8 +18,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +27,58 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+// click scroll
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  console.log(
+    'section1.getBoundingClientRect',
+    section1.getBoundingClientRect()
+  );
+  console.log(
+    'e.target.getBoundingClientRect',
+    e.target.getBoundingClientRect()
+  );
+  console.log('Current scroll X/Y:', window.pageXOffset, pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // scroll
+  // window.scrollTo(
+  //   section1.getBoundingClientRect().left + window.pageXOffset,
+  //   section1.getBoundingClientRect().top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   top: section1.getBoundingClientRect().top + window.pageYOffset,
+  //   left: section1.getBoundingClientRect().left + window.pageXOffset,
+  //   behavior: 'smooth',
+  // });
+  // modern scroll
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// 事件捕获，事件冒泡
+const randomInt = (max, min) =>
+  Math.floor(Math.random() * (max - min) + 1 + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+});
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINKS', e.target, e.currentTarget);
+});
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
 });
