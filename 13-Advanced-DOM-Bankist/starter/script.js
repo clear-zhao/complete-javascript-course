3a875dc5a7eed0dd213867b6d2f1496adb60c735
clear-zhao <1289134886@qq.com>
2023-08-18 22:03:47 +0800

    day 14

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -198,33 +198,80 @@ images.forEach(img => imgObserver.observe(img));
 
 // Slider
 // 该功能就是更改translateX的值
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-let curSlide = 0;
-const maxSlide = slides.length;
+const slider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-const goToSlide = function (slide) {
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activeDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) curSlide = 0;
+    else curSlide++;
+
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+  const prevSlide = function () {
+    if (curSlide === 0) curSlide = maxSlide - 1;
+    else curSlide--;
+
+    goToSlide(curSlide);
+    activeDot(curSlide);
+  };
+
+  // init
+  const init = function () {
+    createDots();
+    activeDot(0);
+    goToSlide(0);
+  };
+  init();
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+  document.addEventListener('keydown', function (e) {
+    e.key === 'ArrowLeft' && prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      // const { slide } = e.target.dataset;
+      const targetSlide = e.target.dataset.slide;
+      curSlide = targetSlide;
+      goToSlide(curSlide);
+      activeDot(curSlide);
+    }
   });
 };
-goToSlide(0);
-
-const nextSlide = function () {
-  if (curSlide === maxSlide - 1) curSlide = 0;
-  else curSlide++;
-
-  goToSlide(curSlide);
-};
-const prevSlide = function () {
-  if (curSlide === 0) curSlide = maxSlide - 1;
-  else curSlide--;
-
-  goToSlide(curSlide);
-};
-btnRight.addEventListener('click', nextSlide);
-btnLeft.addEventListener('click', prevSlide);
+slider();
 //////////////////////////////////////////
 // 事件捕获，事件冒泡
 // const randomInt = (max, min) =>
