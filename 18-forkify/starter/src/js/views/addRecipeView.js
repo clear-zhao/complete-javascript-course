7f8b6efa2ed3db51f2b4874e0a186a38e3c42450
clear-zhao <1289134886@qq.com>
2023-12-27 22:30:48 +0800

    day 31

--- 18-forkify/starter/src/js/views/addRecipeView.js
+++ 18-forkify/starter/src/js/views/addRecipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 
-class addRecipeView extends View {
+class addrecipeView extends View {
   _parentElement = document.querySelector('.upload');
   _message = 'Recipe was successfully uploaded :)';
 
@@ -42,4 +42,4 @@ class addRecipeView extends View {
   _generateMarkup() {}
 }
 
-export default new addRecipeView();
+export default new addrecipeView();
